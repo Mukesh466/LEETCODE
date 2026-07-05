91325da8a90bbd046a888d2b8053e97c9631ed72
Mukesh kumar D <mukeshkumard04062006@gmail.com>
2026-07-05 21:10:02 +0530

    Create Merge sorted array.py

---
+++ Merge sorted array.py
@@ -0,0 +1,6 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        for i in range(n):
+            nums1[m+i]=nums2[i]
+        nums1.sort()
+      #append the value of nums2 to nums1's 0 values and then sorting it in nums1
