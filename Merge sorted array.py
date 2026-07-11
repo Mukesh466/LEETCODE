63d5519f513132366cd64f413b7fa677bad99d44
Mukesh kumar D <mukeshkumard04062006@gmail.com>
2026-07-11 20:46:40 +0530

    Update Merge sorted array.py

--- Merge sorted array.py
+++ Merge sorted array.py
@@ -3,4 +3,5 @@ class Solution:
         for i in range(n):
             nums1[m+i]=nums2[i]
         nums1.sort()
-      #append the value of nums2 to nums1's 0 values and then sorting it in nums1
+      #append the value of nums2 to nums1's 0 values and then sorting it in nums1 so array can be sorted
+
