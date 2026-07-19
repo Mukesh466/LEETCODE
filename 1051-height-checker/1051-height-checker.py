e9e0a6ba90b02cdcb017f60b55760fdfa9b46051
Mukesh kumar D <mukeshkumard04062006@gmail.com>
2026-07-19 13:13:45 +0530

    Time: 3 ms (15.07%), Space: 19.2 MB (52.58%) - LeetHub

--- 1051-height-checker/1051-height-checker.py
+++ 1051-height-checker/1051-height-checker.py
@@ -2,7 +2,7 @@ class Solution:
     def heightChecker(self, heights: List[int]) -> int:
         n=sorted(heights)
         count=0
-        for i in range(len(heights)):
+        for i in range(0,len(heights)):
             if heights[i] != n[i]:
                 count +=1
         return count
