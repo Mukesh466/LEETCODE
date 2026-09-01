5741281d8a647aa67ef00a8d99fbe3f8afff4003
Mukesh kumar D <mukeshkumard04062006@gmail.com>
2026-09-01 18:56:14 +0530

    Time: 1437 ms (43.89%), Space: 0B (100%) - LeetHub

--- 1158-market-analysis-i/1158-market-analysis-i.sql
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -2,5 +2,5 @@
 select u.user_id as buyer_id,u.join_date,count(o.order_id) as orders_in_2019
 from Users u
 left join Orders o
-on u.user_id=o.buyer_id and o.order_date >= "2019-01-01" and o.order_date < "2020-01-01"
-group by u.user_id,u.join_date;
+on u.user_id=o.buyer_id and year(order_date) ="2019"
+group by u.user_id;
