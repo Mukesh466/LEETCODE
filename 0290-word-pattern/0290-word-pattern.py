5663187b42056093f32b8e4fc77743866780de9f
Mukesh kumar D <mukeshkumard04062006@gmail.com>
2026-09-03 23:18:46 +0530

    Time: 0 ms (100%), Space: 19.3 MB (30.11%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.py
@@ -0,0 +1,16 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        words=s.split()
+        if len(pattern) != len(words):
+            return False
+        seen={}
+        for p,w in zip(pattern,words):
+            p_key=("p",p)
+            w_key=("w",w)
+            if  p_key in seen and  seen[p_key] != w:
+                return False
+            if w_key in seen and seen[w_key] != p:
+                return False
+            seen[p_key] = w
+            seen[w_key] = p
+        return True 
