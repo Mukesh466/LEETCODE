e23cc7a1f6472ffac71fb6e6b8cb3b2a5b6f798a
Mukesh kumar D <mukeshkumard04062006@gmail.com>
2026-09-14 20:59:11 +0530

    Time: 0 ms (100%), Space: 19.4 MB (24%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        res=[]
+        for ch in s:
+            if ch in "{[(":
+                res.append(ch)
+            else:
+                if not  res:
+                    return False
+                top = res.pop()
+                if ch ==")" and top !="(":
+                    return False
+                if ch =="}" and top !="{":
+                    return False
+                if ch =="]" and top !="[":
+                    return False
+        return not res
